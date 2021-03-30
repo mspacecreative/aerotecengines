0455adcecd202787877d6f66f4f51fadc4027b78
Matt <matt@mspacecreative.com>
2021-03-30 15:08:17 -0300

    Formatting zoho form

---
+++ includes/zoho-form.php
@@ -0,0 +1,431 @@
+<div id='crmWebToEntityForm' class='zcwf_lblRight crmWebToEntityForm' style='background-color: white;color: #575757;max-width: 600px;'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	
+	<META HTTP-EQUIV ='content-type' CONTENT='text/html;charset=UTF-8'>
+		
+	<form action='https://crm.zoho.com/crm/WebToLeadForm' name=WebToLeads1559003000002635293 method='POST' onSubmit='javascript:document.charset="UTF-8"; return checkMandatory1559003000002635293()' accept-charset='UTF-8'>
+		
+		<input type='text' style='display:none;' name='xnQsjsdp' value='60a18ed8e00ad3b22963525e02f26db98aa58f54593604a67d2f59ef975fa309'>
+		</input>
+		
+		<input type='hidden' name='zc_gad' id='zc_gad' value=''>
+		</input>
+		
+		<input type='text' style='display:none;' name='xmIwtLD' value='0c2dfd15f496aa4ca8935009094158734ffeecf31b02a07a343492d21ff3b232'>
+		</input>
+		
+		<input type='text'  style='display:none;' name='actionType' value='TGVhZHM='>
+		</input>
+		
+		<input type='text' style='display:none;' name='returnURL' value='https://aerotecengines.ca/' >
+		</input>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF104'>Engine Exchange</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='checkbox' class='zcwf_ckbox' id='LEADCF104' name='LEADCF104' maxlength='3'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF102'>Engine Overhaul</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='checkbox' class='zcwf_ckbox' id='LEADCF102' name='LEADCF102' maxlength='3'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF109'>Engine Repair &#x2f; IRAN</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='checkbox' class='zcwf_ckbox' id='LEADCF109' name='LEADCF109' maxlength='3'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF106'>Prop Strike</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='checkbox' class='zcwf_ckbox' id='LEADCF106' name='LEADCF106' maxlength='3'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF3'>Engine Model (include all pre/suffixes)<span style='color:red;'>*</span></label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='LEADCF3' name='LEADCF3' maxlength='250'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF10'>Engine S&#x2f;N</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='LEADCF10' name='LEADCF10' maxlength='255'>
+				</input>
+				
+				<div class='zcwf_col_help'><span title='The serial number on your engine' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>The serial number on your engine</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF13'>Hours Since Last Overhaul</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='LEADCF13' name='LEADCF13' maxlength='255'>
+				</input>
+				
+				<div class='zcwf_col_help'><span title='How many hours are on your engine since the last overhaul' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>How many hours are on your engine since the last overhaul</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF12'>Years Since Last Overhaul</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<select class='zcwf_col_fld_slt' id='LEADCF12' name='LEADCF12'  >
+					<option value='-None-'>-None-</option>
+					
+					<option value='0'>0</option>
+					
+					<option value='1'>1</option>
+					
+					<option value='2'>2</option>
+					
+					<option value='3'>3</option>
+					
+					<option value='4'>4</option>
+					
+					<option value='5'>5</option>
+					
+					<option value='6'>6</option>
+					
+					<option value='7'>7</option>
+					
+					<option value='8'>8</option>
+					
+					<option value='9'>9</option>
+					
+					<option value='10'>10</option>
+					
+					<option value='11'>11</option>
+					
+					<option value='12'>12</option>
+					
+					<option value='13'>13</option>
+					
+					<option value='14'>14</option>
+					
+					<option value='15'>15</option>
+					
+					<option value='16'>16</option>
+					
+					<option value='17'>17</option>
+					
+					<option value='18'>18</option>
+					
+					<option value='19'>19</option>
+					
+					<option value='20'>20</option>
+					
+					<option value='21'>21</option>
+					
+					<option value='22'>22</option>
+					
+					<option value='23'>23</option>
+					
+					<option value='24'>24</option>
+					
+					<option value='25'>25</option>
+					
+					<option value='26'>26</option>
+					
+					<option value='27'>27</option>
+					
+					<option value='28'>28</option>
+					
+					<option value='29'>29</option>
+					
+					<option value='30'>30</option>
+					
+					<option value='31'>31</option>
+					
+					<option value='32'>32</option>
+					
+					<option value='33'>33</option>
+					
+					<option value='34'>34</option>
+					
+					<option value='35'>35</option>
+					
+					<option value='36'>36</option>
+					
+					<option value='37'>37</option>
+					
+					<option value='38'>38</option>
+					
+					<option value='39'>39</option>
+					
+					<option value='40'>40</option>
+					
+					<option value='41'>41</option>
+					
+					<option value='42'>42</option>
+					
+					<option value='43'>43</option>
+					
+					<option value='44'>44</option>
+					
+					<option value='45'>45</option>
+					
+					<option value='46'>46</option>
+					
+					<option value='47'>47</option>
+					
+					<option value='48'>48</option>
+					
+					<option value='49'>49</option>
+					
+					<option value='50'>50</option>
+				</select>
+				
+				<div class='zcwf_col_help'><span title='How many years it has been since your engine was last overhauled' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>How many years it has been since your engine was last overhauled</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF4'>Tail Registration Number<span style='color:red;'>*</span></label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='LEADCF4' name='LEADCF4' maxlength='250'>
+				</input>
+				
+				<div class='zcwf_col_help'><span title='This is your airframe registration number' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>This is your airframe registration number</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='First_Name'>First Name<span style='color:red;'>*</span></label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='First_Name' name='First Name' maxlength='40'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='Last_Name'>Last Name<span style='color:red;'>*</span></label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='Last_Name' name='Last Name' maxlength='80'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='Company'>Company</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='Company' name='Company' maxlength='100'>
+				</input>
+				
+				<div class='zcwf_col_help'><span title='Complete this field if requesting a quote for a company' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>Complete this field if requesting a quote for a company</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='Email'>Email</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' ftype='email' id='Email' name='Email' maxlength='100'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='Phone'>Phone</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='text' id='Phone' name='Phone' maxlength='30'>
+				</input>
+				
+				<div class='zcwf_col_help'></div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab' style='font-size:16px; font-family: Calibri;'>
+				<label for='LEADCF7'>Additional Comments &#x2f; Questions</label>
+			</div>
+			
+			<div class='zcwf_col_fld'>
+				<textarea id='LEADCF7' name='LEADCF7'></textarea>
+				
+				<div class='zcwf_col_help'><span title='Ex&#x3a; Airframe Model, Airframe S&#x2f;N, Airframe Voltage, Shipping Requirements, etc' style='cursor: pointer; width: 16px; height: 16px; display: inline-block; background: #fff; border: 1px solid #ccc; color: #ccc; text-align: center; font-size: 11px; line-height: 16px; font-weight: bold; border-radius: 50%;' onclick='tooltipShow1559003000002635293(this)'>?</span>
+					
+					<div class='zcwf_tooltip_over' style='display: none;'><span class='zcwf_tooltip_ctn'>Ex&#x3a; Airframe Model, Airframe S&#x2f;N, Airframe Voltage, Shipping Requirements, etc</span></div>
+				</div>
+			</div>
+		</div>
+		
+		<div class='zcwf_row'>
+			<div class='zcwf_col_lab'></div>
+			
+			<div class='zcwf_col_fld'>
+				<input type='submit' id='formsubmit' class='formsubmit zcwf_button' value='Submit&#x20;Request' title='Submit&#x20;Request'>
+				
+				<input type='reset' class='zcwf_button' name='reset' value='Reset' title='Reset'>
+			</div>
+		</div>
+		
+		<script>
+		function validateEmail1559003000002635293() {
+			var form = document.forms['WebToLeads1559003000002635293'];
+			var emailFld = form.querySelectorAll('[ftype=email]');
+			var i;
+			for (i = 0; i < emailFld.length; i++) {
+				var emailVal = emailFld[i].value;
+				if ((emailVal.replace(/^\s+|\s+$/g, '')).length != 0) {
+					var atpos = emailVal.indexOf('@');
+					var dotpos = emailVal.lastIndexOf('.');
+					if (atpos < 1 || dotpos <atpos + 2 || dotpos + 2 >= emailVal.length) {
+						alert('Please enter a valid email address. ');
+						emailFld[i].focus();
+						return false;
+					}
+				}
+			}
+			
+			return true;
+		}
+		
+		function checkMandatory1559003000002635293() {
+			var mndFileds = new Array('First Name', 'Last Name', 'LEADCF3', 'LEADCF4');
+			var fldLangVal = new Array('First\x20Name', 'Last\x20Name', 'Engine\x20Model\x20\x28include\x20all\x20pre\x2Fsuffixes\x29', 'Tail\x20Registration\x20Number');
+			for (i = 0; i < mndFileds.length; i++) {
+				var fieldObj = document.forms['WebToLeads1559003000002635293'][mndFileds[i]];
+				if (fieldObj) {
+					if (((fieldObj.value).replace(/^\s+|\s+$/g, '')).length == 0) {
+						if (fieldObj.type == 'file') {
+							alert('Please select a file to upload.');
+							fieldObj.focus();
+							return false;
+						}
+						
+						alert(fldLangVal[i] + ' cannot be empty.');
+						fieldObj.focus();
+						return false;
+					} else if (fieldObj.nodeName == 'SELECT') {
+						if (fieldObj.options[fieldObj.selectedIndex].value == '-None-') {
+							alert(fldLangVal[i] + ' cannot be none.');
+							fieldObj.focus();
+							return false;
+						}
+					} else if (fieldObj.type == 'checkbox') {
+						if (fieldObj.checked == false) {
+							alert('Please accept  ' + fldLangVal[i]);
+							fieldObj.focus();
+							return false;
+						}
+					}
+					
+					try {
+						if (fieldObj.name == 'Last Name') {
+							name = fieldObj.value;
+						}
+					} catch (e) {}
+				}
+			}
+			
+			if (!validateEmail1559003000002635293()) {
+				return false;
+			}
+			
+			document.querySelector('.crmWebToEntityForm .formsubmit').setAttribute('disabled', true);
+		}
+		
+		function tooltipShow1559003000002635293(el) {
+			var tooltip = el.nextElementSibling;
+			var tooltipDisplay = tooltip.style.display;
+			if (tooltipDisplay == 'none') {
+				var allTooltip = document.getElementsByClassName('zcwf_tooltip_over');
+				for (i = 0; i < allTooltip.length; i++) {
+					allTooltip[i].style.display = 'none';
+				}
+				
+				tooltip.style.display = 'block';
+			} else {
+				tooltip.style.display = 'none';
+			}
+		}
+		</script>
+	
+	</form>
+</div>
